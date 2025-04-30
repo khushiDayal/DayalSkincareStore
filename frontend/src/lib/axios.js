6835d2f1f9e3de12ccca7d1a82e1ea7083c253fb
Khushi Dayal <149425839+khushiDayal@users.noreply.github.com>
2025-04-30 12:35:57 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.mode === "development" ? "https://dayalskincarestore.onrender.com" : "/api",
+    baseURL: import.meta.mode === "development" ? "https://dayalskincarestore.onrender.com" : "http://localhost:5000/api",
     withCredentials: true, //send cookies to the server
 });
 
