c5141a2be6a14a04f0be99075113289bddc8b4da
Khushi Dayal <149425839+khushiDayal@users.noreply.github.com>
2025-04-30 12:48:45 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.mode === "development"
-        ? "http://localhost:5000/api" // local development
-        : "https://dayalskincarestore.onrender.com/api", // production
+    baseURL: import.meta.env.MODE === "development"
+        ? "http://localhost:5000/api"
+        : "https://dayalskincarestore.onrender.com/api",
     withCredentials: true,
 });
-
 
 export default axiosInstance;
